01162e9658b555058a16a115191bbcde1ec6b115
NullCoke <nullcoke@gmail.com>
2023-04-19 18:01:31 +0800

    test: add Num case

--- src/test/java/com/leptjson/Test.java
+++ src/test/java/com/leptjson/Test.java
@@ -6,8 +6,7 @@ public class Test {
 
     @org.junit.Test
     public void test() {
-        String s = "3.1416";
-        System.out.println(Double.parseDouble(s));
-        System.out.println(Numeric.subNumber(s));
+        String s = "inf  ";
+        System.out.println(s.indexOf(' '));
     }
 }
