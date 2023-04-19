d7a719aff94f2d973536a5860f2ac661f30d3081
NullCoke <nullcoke@gmail.com>
2023-04-19 20:20:20 +0800

    add: getter and setter

--- src/main/java/com/leptjson/LeptValue.java
+++ src/main/java/com/leptjson/LeptValue.java
@@ -3,13 +3,35 @@ package com.leptjson;
 public class LeptValue {
     LeptType type;
     double n;
+    String s;
 
     public LeptType getType() {
         return type;
     }
 
+    public int getBool() {
+        return 0;
+    }
+
+    public void setBool() {
+
+    }
+
     public double getNumber() {
         assert type == LeptType.NUMBER;
         return n;
+    }
+
+    public void setNumber(LeptValue v, double n) {
+
+    }
+
+    public String getString() {
+        return this.s;
+    }
+
+    public void setString(LeptValue v, String s) {
+        v.type = LeptType.STRING;
+        this.s = s;
     }
 }
