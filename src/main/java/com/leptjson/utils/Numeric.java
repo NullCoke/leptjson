14b3c739e64f5ebd2e8cd27ea14443cdff60c84a
NullCoke <nullcoke@gmail.com>
2023-04-19 16:51:56 +0800

    fix: Negative numbers are supported

--- src/main/java/com/leptjson/utils/Numeric.java
+++ src/main/java/com/leptjson/utils/Numeric.java
@@ -4,11 +4,16 @@ import com.leptjson.JsonContext;
 
 public class Numeric {
     public static int subNumber(String s) {
-        if (!Character.isDigit(s.charAt(0)) && s.charAt(0) != '.') {
+        int i = 0;
+        boolean negative = false;
+        if (!Character.isDigit(s.charAt(0)) && s.charAt(0) != '-') {
             return 0;
         }
+        if (s.charAt(0) == '-') {
+            negative = true;
+            i++;
+        }
         int count = 0;
-        int i = 0;
         while (i < s.length() && count < 2) {
             if (s.charAt(i) == '.') {
                 count++;
